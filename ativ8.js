1231ebc7e235d61f3ae8dd9d5fdf93a3dfc7ed9c
Elias Araujo <130007507+eliasdevmind@users.noreply.github.com>
2024-04-05 18:58:23 -0300

    Atualizar o ativ8.js

--- ativ8.js
+++ ativ8.js
@@ -1,6 +1,6 @@
 
 function calculoDaMulta(diasDeMulta, valorDaMulta ){
-    multaTotatal = diasDeMulta * valorDaMulta;
+    let multaTotatal = diasDeMulta * valorDaMulta;
     console.log(`Você atrasol ${diasDeMulta} com multa de valor R$: ${valorDaMulta} por dia o total que você tem apagar de multa é R$: ${multaTotatal} `);
 
 };
